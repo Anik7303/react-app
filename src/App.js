fce6e46a36abe8598b4493cd6a0ceba8b5069c35
Anik7303 <anik7303@gmail.com>
2021-05-17 20:38:38 +0600

    styled modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import { useState, useCallback } from 'react'
+import styled from 'styled-components'
 
 import Modal from './Modal'
 
+const Container = styled.div`
+    text-align: center;
+`
+
+const ShowModalButton = styled.button`
+    background-color: green;
+    color: white;
+    padding: 1rem;
+    font-size: 1.6rem;
+    border: 2px solid green;
+    cursor: pointer;
+    transition: background 200ms, color 200ms;
+    box-shadow: 0 5px 20px 2px rgba(0, 0, 0, 0.2);
+
+    &:hover {
+        background-color: white;
+        color: green;
+    }
+`
+
 function App() {
+    const [error, setError] = useState(null)
+    const onClose = useCallback(() => setError(null), [])
+    const showModal = useCallback(() => setError('There was an error'), [])
+
     return (
-        <div>
-            <h1>Welcome to React</h1>
-            <p>Testing react portal</p>
-            <Modal />
-        </div>
+        <Container>
+            <h1>Welcome to React 17</h1>
+            <p>Testing React Features:</p>
+            <ul>
+                <li>Portal</li>
+                <li>Error Boundary</li>
+            </ul>
+            <ShowModalButton onClick={showModal}>Show Modal</ShowModalButton>
+            <Modal show={error !== null} onClose={onClose} />
+        </Container>
     )
 }
 
--- src/Modal.js
+++ src/Modal.js
@@ -1,8 +1,41 @@
-import React from 'react'
 import { createPortal } from 'react-dom'
+import styled from 'styled-components'
 
-function Modal({ children }) {
-    const element = <div>{children}</div>
+const ModalContainer = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: 90vw;
+    max-width: 40rem;
+    height: 50vh;
+    max-height: 40rem;
+    padding: 2rem;
+    background-color: white;
+    box-shadow: 0 5px 20px 2px rgba(0, 0, 0, 0.2);
+    z-index: 2;
+`
+
+const Backdrop = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+`
+
+function Modal({ children, show, onClose }) {
+    if (!show) return null
+    const element = (
+        <>
+            <Backdrop onClick={onClose} />
+            <ModalContainer>{children}</ModalContainer>
+        </>
+    )
     return createPortal(element, document.getElementById('modal'))
 }
 
